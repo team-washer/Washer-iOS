aa98afe815d8de27768dc74732bc0c39c1832fb4
“xixn2” <ssbkh0802@naver.com>
2025-04-08 15:45:35 +0900

    ✨ :: [#11] Email / Password 정규식 확인 기능 추가

---
+++ Projects/App/Sources/Extension/Validator.swift
@@ -0,0 +1,24 @@
+//
+//  Validator.swift
+//  Washer
+//
+//  Created by 서지완 on 4/8/25.
+//  Copyright © 2025 Washer. All rights reserved.
+//
+
+import Foundation
+
+struct Validator {
+    static let emailRegex = "^s\\d{5}$"
+    static let passwordRegex = "^(?=.*[^A-Za-z0-9]).{8,16}$"
+
+    static func isValidEmail(_ email: String) -> Bool {
+        NSPredicate(format: "SELF MATCHES %@", emailRegex)
+            .evaluate(with: email)
+    }
+
+    static func isValidPassword(_ password: String) -> Bool {
+        NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+            .evaluate(with: password)
+    }
+}
--- Projects/App/Sources/Feature/AuthFeature/SignInFeature/View/SignInView.swift
+++ Projects/App/Sources/Feature/AuthFeature/SignInFeature/View/SignInView.swift
@@ -11,14 +11,24 @@ import SwiftUI
 struct SignInView: View {
     @State private var emailTextField: String = ""
     @State private var passwordTextField: String = ""
-    @State private var showError: Bool = false
+    @State private var emailShowError: Bool = false
+    @State private var passwordShowError: Bool = false
+    @State private var isLoggedIn: Bool = false
+
     var body: some View {
         VStack(spacing: 0) {
-            Button {
-                showError.toggle()
-            } label: {
-                Text("오류 발생!!버튼!!")
-            }.padding(.bottom, 100)
+            HStack {
+                WasherAsset.washerLeftButton.swiftUIImage
+                    .padding(.leading, 16)
+                    .padding(.top, 34)
+
+                Spacer()
+            }
+
+            HStack {
+                WasherAsset.washerLogo.swiftUIImage
+                    .padding(.top, 5)
+            }
 
             HStack(spacing: 0) {
                 WasherTextField (
@@ -26,8 +36,9 @@ struct SignInView: View {
                     text: $emailTextField,
                     title: "이메일",
                     errorText: "이메일 형식이 맞지 않습니다.",
-                    isError: showError
+                    isError: emailShowError
                 )
+
                 Text("@")
                     .font(.pretendard(.medium, size: 18))
                     .color(.gray400)
@@ -46,6 +57,34 @@ struct SignInView: View {
                     .padding(.leading, 16)
                     .padding(.top, 20)
             }
+            .padding(.top, 64)
+
+            WasherTextField (
+                "비밀번호를 입력해주세요.",
+                text: $passwordTextField,
+                title: "비밀번호",
+                errorText: "비밀번호 형식이 맞지 않습니다.",
+                isError: passwordShowError
+            )
+            .padding(.top, 34)
+
+            Button {
+                isLoggedIn.toggle()
+            } label: {
+                HStack(spacing: 2) {
+                    Image(isLoggedIn ? "washerCheckButton" : "washerNoneCheckButton")
+
+                    Text("로그인 상태 유지")
+                        .font(.pretendard(.medium, size: 12))
+                        .color(isLoggedIn ? .gray500 : .gray300)
+
+                    Spacer()
+                }
+                .padding(.leading, 16)
+                .padding(.top, 6)
+            }
+
+            Spacer()
         }
     }
 }
