64bca39e31b8caa893eb47b8b7ef5d23bd8e0e89
“xixn2” <ssbkh0802@naver.com>
2025-04-09 11:34:39 +0900

    💄 :: [#13] 회원가입 화면 제작

--- Projects/App/Sources/Feature/AuthFeature/SignUpFeature/View/SignUpView.swift
+++ Projects/App/Sources/Feature/AuthFeature/SignUpFeature/View/SignUpView.swift
@@ -10,42 +10,74 @@ import SwiftUI
 
 struct SignUpView: View {
     @StateObject var authViewModel: AuthViewModel
+    @State var nameTextField: String = ""
+    @State var nameIsError: Bool = false
+    @State var schoolNumberTextField: String = ""
+    @State var schoolNumberIsError: Bool = false
+    @State var domitoryRoomTextField: String = ""
+    @State var domitoryRoomIsError: Bool = false
 
     var body: some View {
-        VStack {
-            Button {
-                authViewModel.setupEmail(email: "s23053")
-                authViewModel.setupPassword(password: "washertest1!")
-                authViewModel.setupName(name: "서지완")
-                authViewModel.setupGrade(grade: "3")
-                authViewModel.setupClassRoom(classRoom: "3")
-                authViewModel.setupNumber(number: "14")
-                authViewModel.setupGender(gender: "MAN")
-                authViewModel.setupRoom(room: "415")
-                authViewModel.signUp { statusCode in
-                    if (200...299).contains(statusCode) {
-                        print("\(statusCode) | 회원가입 성공")
-                    } else {
-                        print("\(statusCode) | 회원가입 실패")
-                    }
-                }
-            } label: {
-                Text("회원가입 테스트 버튼")
-            }
+        VStack(spacing: 34) {
+            ZStack {
+                HStack {
+                    WasherAsset.washerLeftButton.swiftUIImage
+                        .padding(.leading, 26)
 
-            Button {
-                authViewModel.setupEmail(email: "s23053")
-                authViewModel.setupPassword(password: "washertest1!")
-                authViewModel.signIn { statusCode in
-                    if (200...299).contains(statusCode) {
-                        print("\(statusCode) | 로그인 성공")
-                    } else {
-                        print("\(statusCode) | 로그인 실패")
-                    }
+                    Spacer()
                 }
-            } label: {
-                Text("로그인 테스트 버튼")
+
+                Spacer()
+
+                VStack(spacing: 6) {
+                    Text("회원가입")
+                        .font(.pretendard(.bold, size: 18))
+                        .foregroundStyle(.black)
+
+                    Text("로그인 시 사용할 정보를 입력해주세요")
+                        .font(.pretendard(.regular, size: 12))
+                        .color(.gray300)
+                }
+
             }
+            .padding(.top, 50)
+
+            WasherTextField(
+                "이름을 입력해주세요",
+                text: $nameTextField,
+                title: "이름",
+                errorText: "이름을 잘못 입력했습니다.",
+                isError: nameIsError
+            )
+            .padding(.top, 38)
+
+            WasherTextField(
+                "학번을 입력해주세요",
+                text: $schoolNumberTextField,
+                title: "학번",
+                errorText: "숫자 4자리만 입력이 가능합니다. (ex.3314)",
+                isError: schoolNumberIsError
+            )
+
+            WasherTextField(
+                "기숙사 호실을 입력해주세요",
+                text: $domitoryRoomTextField,
+                title: "호실",
+                errorText: "숫자 3자리만 입력이 가능합니다. (ex.415)",
+                isError: domitoryRoomIsError
+            )
+
+            WasherButton(
+                text: "다음",
+                horizontalPadding: 173,
+                verticalPadding: 17
+            )
+
+            Spacer()
         }
     }
+}
+
+#Preview {
+    SignUpView(authViewModel: AuthViewModel())
 }
